32c483d2520afe6f0a97798c4b334a569e808de5
Gustav Schenck <gugi.schenck@icloud.com>
2023-06-07 10:08:07 +0200

    questionnaire706

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,37 +1,35 @@
 class QuestionnairesController < ApplicationController
   def new
-    @questionaire = Questionaire.new
+    @questionnaire = Questionnaire.new
     authorize(@questionnaire)
   end
 
   def create
-<<<<<<< HEAD
-    @questionaire = Questionaire.new(questionnare_params)
+    @questionnaire = Questionnaire.new(questionnaire_params)
 
-=======
-    @questionaire = Questionaire.new(article_params)
->>>>>>> a3845b4a0e11daf902000f354edd5ee134dd4857
     respond_to do |format|
       if @questionaire.save
-        format.html { redirect_to new_questionaire_path(@questionaire), notice: "Restart your questionaire" }
-        format.json { render :show, status: :created, location: @questionaire }
+        format.html { redirect_to new_questionnaire_path(@questionnaire), notice: "Restart your questionnaire" }
+        format.json { render :show, status: :created, location: @questionnaire }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @questionaire.errors, status: :unprocessable_entity }
+        format.json { render json: @questionnaire.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def index
-    @questionaire = Questionaire.all
+    @questionnaire = Questionnaire.all
   end
 
   def destroy
-    @questionaire.destroy
+    @questionnaire.destroy
 
     respond_to do |format|
-      format.html { redirect_to new_questionaire_path , notice: "Restart your questionaire" }
+      format.html { redirect_to new_questionnaire_path , notice: "Restart your questionnaire" }
       format.json { head :no_content }
     end
   end
+
+
 end
